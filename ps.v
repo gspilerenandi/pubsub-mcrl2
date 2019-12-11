8439a05bc492977db9bff5a5c53f84b788f63f3e
gspilerenandi <giannnandi@gmail.com>
2019-12-11 16:32:26 +0000

    fixed some definitions and some variables are now parameters

--- ps.v
+++ ps.v
@@ -7,7 +7,10 @@ Notation "x ⊆ y" := (Included _ x y)(at level 31).
 (*------------------------------------------------------------------------------------------------
   https://stackoverflow.com/questions/56201111/what-is-the-standard-cartesian-product-construction-for-ensemble
 -------------------------------------------------------------------------------------------------*)
-Definition prod_cart (U V : Type) (A : Ensemble U) (B : Ensemble V) 
+Definition prod_cart
+           (U V : Type)
+           (A : Ensemble U)
+           (B : Ensemble V) 
   : Ensemble (U * V)
   := fun x => In _ A (fst x) /\ In _ B (snd x).
 
@@ -24,17 +27,32 @@ Qed.
 
 (*Definition 1: set of data-values*)
 Variable  data_value: Type.
-Definition d_in_D (d: data_value)(D: Ensemble data_value) := d ∈ D.
+Definition d_in_D
+           (d: data_value)
+           (D: Ensemble data_value)
+  := d ∈ D.
 
 (*Definition 2: timestamps*)
-Definition t_in_T (t: nat)(T: Ensemble nat) := t ∈ T.
+Definition t_in_T
+           (t: nat)
+           (T: Ensemble nat)
+  := t ∈ T.
 
 (*Definition 3: Events*)
-Definition e (d: data_value)(t: nat): data_value*nat := (d,t).
+Definition e
+           (d: data_value)
+           (t: nat)
+  : data_value*nat
+  := (d,t).
 
-Definition e_in_E (e:data_value*nat)(E: Ensemble(data_value*nat)) :=  e ∈ E.
+Definition e_in_E
+           (e:data_value*nat)
+           (E: Ensemble(data_value*nat))
+  :=  e ∈ E.
 
-Definition DxT (D: Ensemble data_value)(T: Ensemble nat)
+Definition DxT
+           (D: Ensemble data_value)
+           (T: Ensemble nat)
   : Ensemble(data_value*nat)
   := prod_cart (data_value)(nat)(D)(T).
 
@@ -42,25 +60,39 @@ Definition E_included_DxT
            (E:Ensemble(data_value*nat))
            (D: Ensemble data_value)
            (T: Ensemble nat)
-           (*(DxT:prod_cart (data_value)(nat)(D: Ensemble data_value)(T: Ensemble nat))*)
   : Prop
   := E ⊆ DxT D T.
 
 (*Definition 4: Event-buffers*)
-Definition b_included_E (b: Ensemble (data_value*nat))(E: Ensemble(data_value*nat)) := b ⊆ E.
+Definition b_included_E
+           (b: Ensemble (data_value*nat))
+           (E: Ensemble(data_value*nat))
+  := b ⊆ E.
 
 (*Definition 5: Topics*)
 Variable topic : Type.
 
-Definition τ_in_Tau (τ:topic)(Tau: Ensemble topic) := τ ∈ Tau.
+Definition τ_in_Tau
+           (τ:topic)
+           (Tau: Ensemble topic)
+  := τ ∈ Tau.
 
 (*Definition 6: Locations*)
 Variable location : Type.
-Definition l_in_L (l:location)(L: Ensemble location) := l ∈ L.
+Definition l_in_L
+           (l:location)
+           (L: Ensemble location)
+  := l ∈ L.
 
 (*Definition 7: Subscribers*)
 
-Definition s (l:location) (ψl:Ensemble topic) (Tau: Ensemble topic) (L: Ensemble location) (P: (ψl ⊆ Tau ) /\ (l_in_L l L)) :=  (l,ψl).
+Definition s
+           (l:location)
+           (ψl:Ensemble topic)
+           (Tau: Ensemble topic)
+           (L: Ensemble location)
+           (P: (ψl ⊆ Tau ) /\ (l_in_L l L))
+  :=  (l,ψl).
 
 
 
